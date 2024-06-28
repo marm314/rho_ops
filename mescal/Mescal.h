d34470af6ea263ab274678688f299d001c97758a
Mauricio Rodriguez-Mayorga <marm3.14@gmail.com>
2024-06-28 21:46:15 +0200

    Put Vreac in mescal object

--- mescal/Mescal.h
+++ mescal/Mescal.h
@@ -52,8 +52,8 @@ class Mescal
   ~Mescal();
   string sha="";
   bool perm_q=false,ind_q=false,part_val_e=false,mute=true,deact_rad=false;
-  int nfragments,maxiter=1000,iter=0,ifrac_deact=-1,nthread;
-  double r0=0.0e0,w_damp=0.4,mu_diff_max,q_diff_max,E_diff,threshold_mu,threshold_E,threshold_q,Energy;
+  int nfragments,maxiter=1000,iter=0,ifrac_deact=-1,nthread,naux_basis;
+  double r0=0.0e0,w_damp=0.4,mu_diff_max,q_diff_max,E_diff,threshold_mu,threshold_E,threshold_q,Energy,**Vreac;
   struct FRAGMENT
   {
    string name;
